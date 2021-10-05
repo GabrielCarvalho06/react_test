bf64def6a0beaaf22a21f3e3da69063c2de23ef3
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-04 23:55:56 -0300

    Inserindo ImagePicker e permissão para galeria

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permissão é necessária para acessar as mídias')
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Image source = {logo}
+              style = {styles.logo}/>
+      <Text style = {styles.instructions}>Para compartilhar uma foto, você deve clicar no botão abaixo!!!</Text>
       <StatusBar style="auto" />
+
+      <TouchableOpacity 
+        onPress={openImagePickerAsync}
+        style = {styles.button}>
+          <Text style={styles.buttonText}>Escolha uma foto</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -14,10 +38,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#444',
     alignItems: 'center',
     justifyContent: 'center',
-    color:'white',
-     
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom:10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor:'blue',
+    padding:50,
+    borderRadius:100,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }
 });
