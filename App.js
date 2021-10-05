7149445eee54f4bbb9e54ac7ba68e6127c45adef
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-05 00:56:42 -0300

    Selecionando imagem da galeria e mostrando ela na tela inicial

--- App.js
+++ App.js
@@ -1,40 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+  const [selectedImage, setSelectImage] = React.useState(null);
+
   let openImagePickerAsync = async () => {
     
     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
     if (permissionResult.granted === false) {
       alert('Permissão é necessária para acessar as mídias')
-      return;
+      return
     }
 
     let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
-  }
+    //console.log(pickerResult);
+    if(pickerResult.cancelled === true) {
+      return
+    }
 
-  return (
+    setSelectImage({localUri: pickerResult.uri})
+  };
+
+  if(selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+
+        <Image source = {{ uri: selectedImage.localUri}}
+            style = {styles.thumbnail}/>
+
+        <Image source = {logo}
+            style = {styles.logo}/>
+
+        <Text style = {styles.instructions}>Para compartilhar uma foto, você deve clicar no botão abaixo!!!</Text>
+        <StatusBar style="auto" />
+
+        <TouchableOpacity 
+        onPress={openImagePickerAsync}
+        style = {styles.button}>
+          <Text style={styles.buttonText}>Escolha uma foto</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  return(
     <View style={styles.container}>
       <Image source = {logo}
-              style = {styles.logo}/>
-      <Text style = {styles.instructions}>Para compartilhar uma foto, você deve clicar no botão abaixo!!!</Text>
-      <StatusBar style="auto" />
+                style = {styles.logo}/>
+        <Text style = {styles.instructions}>Para compartilhar uma foto, você deve clicar no botão abaixo!!!</Text>
+        <StatusBar style="auto" />
 
       <TouchableOpacity 
         onPress={openImagePickerAsync}
         style = {styles.button}>
           <Text style={styles.buttonText}>Escolha uma foto</Text>
       </TouchableOpacity>
-
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -61,5 +88,10 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: '#fff',
-  }
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  },
 });
